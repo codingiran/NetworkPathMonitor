b5fe5157685d8aa63e3ca26362bb9255ad2e6ded
CodingIran <codingiran@gmail.com>
2025-08-01 16:15:19 +0800

    Release 0.1.5

--- Sources/NetworkPathMonitor/NetworkPathMonitor.swift
+++ Sources/NetworkPathMonitor/NetworkPathMonitor.swift
@@ -21,7 +21,7 @@ import Network
 
 public enum NetworkPathMonitorInfo: Sendable {
     /// Current NetworkPathMonitor version.
-    public static let version = "0.1.4"
+    public static let version = "0.1.5"
 }
 
 /// A class that monitors network path changes using `NWPathMonitor`.
